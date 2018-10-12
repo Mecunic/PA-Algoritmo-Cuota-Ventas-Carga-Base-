713ab788c1e81a68d2e44ff46e2798401e248aed
Mijail Espadas <mijail.espadas@ensitech.com>
2018-10-12 15:05:15 -0500

    Implementación de proyecto base Jobs con Hangfire

---
+++ PlantillaMVC.Jobs/Jobs/DemoJob.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace PlantillaMVC.Jobs.Jobs
+{
+    public class DemoJob
+    {
+        static bool executing = false;
+        static readonly Object thisLock = new Object();
+
+        public static void DemoMethod()
+        {
+            bool NotificationProcessEnabled = true;
+            Boolean.TryParse(System.Configuration.ConfigurationManager.AppSettings["Jobs.EnabledJobs"], out NotificationProcessEnabled);
+            if (Monitor.TryEnter(thisLock))
+            {
+                try
+                {
+                    if (!executing && NotificationProcessEnabled)
+                    {
+
+                        //TODO: Implementar logica de negocio especifica
+                        Trace.TraceInformation(string.Format("[DemoJob.DemoMethod] Executing at {0}", DateTime.Now));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    Trace.TraceInformation(ex.Message);
+                }
+                finally
+                {
+                    executing = false;
+                    Monitor.Exit(thisLock);
+                }
+            }
+        }
+    }
+}
