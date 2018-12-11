33d13e43f2ff238ea96309dfbfe1d2ba4c81aa3a
archer <vieyrafc@gmail.com>
2018-12-11 11:04:16 -0600

    Eliminación de archivos ya no requeridos

--- MVC_Project.Web/Controllers/UserController.cs
+++ MVC_Project.Web/Controllers/UserController.cs
@@ -1,23 +1,27 @@
 ﻿using MVC_Project.Domain.Services;
-using MVC_Project.Models;
+using MVC_Project.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MVC_Project.Web.Controllers {
-
-    public class UserController : Controller {
+namespace MVC_Project.Web.Controllers
+{
+    public class UserController : Controller
+    {
         private IUserService _userService;
 
-        public UserController(IUserService userService) {
+        public UserController(IUserService userService)
+        {
             _userService = userService;
         }
 
         // GET: User
-        public ActionResult Index() {
-            var users = _userService.GetAll().Select(user => new UserViewModel {
+        public ActionResult Index()
+        {
+            var users = _userService.GetAll().Select(user => new UserViewModel
+            {
                 Id = user.Id,
                 Name = user.Name,
                 Email = user.Email,
@@ -28,57 +32,73 @@ namespace MVC_Project.Web.Controllers {
         }
 
         // GET: User/Details/5
-        public ActionResult Details(int id) {
+        public ActionResult Details(int id)
+        {
             return View();
         }
 
         // GET: User/Create
-        public ActionResult Create() {
+        public ActionResult Create()
+        {
             return View();
         }
 
         // POST: User/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection) {
-            try {
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
                 // TODO: Add insert logic here
 
                 return RedirectToAction("Index");
-            } catch {
+            }
+            catch
+            {
                 return View();
             }
         }
 
         // GET: User/Edit/5
-        public ActionResult Edit(int id) {
+        public ActionResult Edit(int id)
+        {
             return View();
         }
 
         // POST: User/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection) {
-            try {
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
                 // TODO: Add update logic here
 
                 return RedirectToAction("Index");
-            } catch {
+            }
+            catch
+            {
                 return View();
             }
         }
 
         // GET: User/Delete/5
-        public ActionResult Delete(int id) {
+        public ActionResult Delete(int id)
+        {
             return View();
         }
 
         // POST: User/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection) {
-            try {
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
                 // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
-            } catch {
+            }
+            catch
+            {
                 return View();
             }
         }
--- PlantillaProyectoMVC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlantillaProyectoMVC.MvcApplication" Language="C#" %>
