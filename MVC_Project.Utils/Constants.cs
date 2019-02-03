19f6b7eca382c863754345a8c08a002494a050d2
Mijail Espadas <mijail.espadas@ensitech.com>
2019-02-03 14:16:02 -0600

    Implementamos el proyecto Utils

---
+++ MVC_Project.Utils/Constants.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MVC_Project.Utils
+{
+    public class Constants
+    {
+        public static readonly string AUTHENTICATED_USER_PROFILE = "ddc30589-80ba-48e6-88ec-6454350f2cd7_USER_PROFILE";
+
+        //ROLE CODES
+        public static readonly string ROLE_COMPANY_ADMIN = "SUPER_ADMIN";
+        public static readonly string ROLE_COMPANY_RESOURCE = "ADMIN";
+        public static readonly string ROLE_GLOBAL_PUBLISHER = "COMPANY";
+
+        //public static string STORAGE_MAIN_CONTAINER = ConfigurationManager.AppSettings["StorageMainContainer"];
+
+        public static string DATE_FORMAT_DAY_MONTH = "dd/MM";
+        public static string DATE_FORMAT = "dd/MM/yyyy";
+        public static string TIMEZONE_UTC = "UTC";
+       
+
+        // Uuids de templates sendgrid
+        public static string NOT_TEMPLATE_WELCOME = "51d34567-1960-4501-8e89-e349c18275e6";
+        public static string NOT_TEMPLATE_ACTIVATION = "534f930b-d06f-4aad-a30a-ff42346e7c56";
+        public static string NOT_TEMPLATE_PASSWORDRESET = "d12f6fbe-cab5-485f-95ec-6875d196d49c";
+        
+        // Encabezados de archivo Excel
+        public static string HEADER_USERIDENTIFIER = "Identificador";
+        public static string HEADER_FIRSTNAME = "Nombre";
+        public static string HEADER_LASTNAME = "Apellido Paterno";
+        public static string HEADER_EMAIL = "Correo Electrónico";
+        public static string HEADER_POSITION = "Puesto";
+
+        //Mensajes
+        public static string View_Message = "View.Message";
+        public static string View_Error = "View.Error";
+        
+        // Status de notificaciones
+        public static int PENDING = 0;
+        public static int SENT = 1;
+        public static int ERROR = 2;
+
+        // URLs varias
+        public static string DEFAULT_AVATAR = "https://bootdey.com/img/Content/avatar/avatar1.png";
+       
+    }
+}
---
+++ MVC_Project.Utils/NotificationUtil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlataformaSWC.Utils
+{
+    public class NotificationUtil
+    {
+        public static void SendNotification(List<string> recipients, Dictionary<string, string> customParams, string template)
+        {
+            string hubURL = ConfigurationManager.AppSettings["NotificationHubUrl"];
+            string apiKey = ConfigurationManager.AppSettings["NotificationHubApiKey"];
+            NotificationHubClient.NotificationHubClient client = new NotificationHubClient.NotificationHubClient(hubURL, apiKey);
+            Dictionary<string, string> authParams = new Dictionary<string, string>();
+            authParams["username"] = ConfigurationManager.AppSettings["SendGridUser"];
+            authParams["password"] = ConfigurationManager.AppSettings["SendGridPassword"];
+            NotificationHubModels.NotificationResponse response = client.SendNotification(new NotificationHubModels.NotificationMessage()
+            {
+                Type = "EMAIL",
+                Provider = "SENDGRID",
+                Template = template, //"51d34567-1960-4501-8e89-e349c18275e6",
+                CustomParams = customParams, //new Dictionary<string, string>() { { "param1", userBO.FirstName }, { "param2", "http://localhost:2683/Account/Confirmation?Uuid=" } },
+                Recipients = recipients, //new List<string> { userBO.Email },
+                ProviderAuthParams = authParams,
+            });
+        }
+
+        public static AlternateView InsertImages(AlternateView alternateView, IList<Tuple<MemoryStream, string>> images)
+        {
+            foreach(var item in images)
+            {
+                LinkedResource linkedResource = new LinkedResource(item.Item1, new ContentType("image/jpeg"));
+                linkedResource.ContentId = item.Item2;
+                //linkedResource.TransferEncoding = System.Net.Mime.TransferEncoding.Base64;
+
+                alternateView.LinkedResources.Add(linkedResource);
+            }
+            
+
+            return alternateView;
+        }
+
+        public static string GenerateHTML(AlternateView alternateView, string template)
+        {
+            MailMessage messageTemp = new MailMessage{ Body = template, IsBodyHtml = true };
+            messageTemp.AlternateViews.Add(alternateView);
+            var stream = messageTemp.AlternateViews[0].ContentStream;
+            byte[] byteBuffer = new byte[stream.Length];
+            return System.Text.Encoding.ASCII.GetString(byteBuffer, 0, stream.Read(byteBuffer, 0, byteBuffer.Length));
+            //using (var rd = new StreamReader(stream))
+            //{
+            //    return rd.ReadToEnd();
+            //}
+        }
+    }
+}
