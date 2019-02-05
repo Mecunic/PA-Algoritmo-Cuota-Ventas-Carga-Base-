4a3f81294ba42341bfe68854c9d0349d923faf14
archer <vieyrafc@gmail.com>
2019-02-05 12:54:02 -0600

    Conexiones | Cambio de conectionString para pauntar a Azure
    * Ajustes de rutas en fuentes/iconos
    * Cambio de target para el proyecto Utilis 4.6.1 -> 4.7.1

--- MVC_Project.Data/Helpers/UnitOfWork.cs
+++ MVC_Project.Data/Helpers/UnitOfWork.cs
@@ -7,51 +7,67 @@ using MVC_Project.Data.Mappings;
 using MVC_Project.Domain.Helpers;
 using System;
 
-namespace MVC_Project.Data.Helpers {
-
-    public class UnitOfWork : IUnitOfWork {
+namespace MVC_Project.Data.Helpers
+{
+    public class UnitOfWork : IUnitOfWork
+    {
         private static readonly ISessionFactory _sessionFactory;
         private static Configuration configuration;
         private ITransaction _transaction;
 
         public ISession Session { get; set; }
 
-        static UnitOfWork() {
+        static UnitOfWork()
+        {
             _sessionFactory = Fluently.Configure()
-                .Database(SQLiteConfiguration.Standard.ConnectionString(conection => conection.FromConnectionStringWithKey("testConectionString")))
+                .Database(
+                MsSqlConfiguration.MsSql2012.ConnectionString(
+                    conection => conection.FromConnectionStringWithKey("testConectionString")))
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>())
                 .ExposeConfiguration(cfg => configuration = cfg)
                 .BuildSessionFactory();
         }
 
-        public UnitOfWork() {
-            Session = _sessionFactory.OpenSession();                        
+        public UnitOfWork()
+        {
+            Session = _sessionFactory.OpenSession();
         }
 
-        public void BeginTransaction() {
+        public void BeginTransaction()
+        {
             Session = _sessionFactory.OpenSession();
             _transaction = Session.BeginTransaction();
         }
 
-        public void Commit() {
-            try {
+        public void Commit()
+        {
+            try
+            {
                 if (_transaction != null && _transaction.IsActive)
                     _transaction.Commit();
-            } catch {
+            }
+            catch
+            {
                 if (_transaction != null && _transaction.IsActive)
                     _transaction.Rollback();
 
                 throw;
-            } finally {
+            }
+            finally
+            {
                 Session.Dispose();
             }
         }
 
-        public void Rollback() {
-            try {
+        public void Rollback()
+        {
+            try
+            {
                 if (_transaction != null && _transaction.IsActive)
                     _transaction.Rollback();
-            } finally {
+            }
+            finally
+            {
                 Session.Dispose();
             }
         }
