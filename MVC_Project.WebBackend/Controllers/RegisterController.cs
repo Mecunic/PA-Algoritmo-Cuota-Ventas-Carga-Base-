730ae9dfba9a9fc9c1830b85809fc662256407d5
Yelmy Pech <yelmymc@hotmail.com>
2020-05-11 17:04:51 -0500

    SinReq Se agrego la funcionalidad para el guardado del registro desde el login

--- MVC_Project.WebBackend/Controllers/RegisterController.cs
+++ MVC_Project.WebBackend/Controllers/RegisterController.cs
@@ -1,6 +1,10 @@
-﻿using MVC_Project.WebBackend.Models;
+﻿using MVC_Project.Domain.Entities;
+using MVC_Project.Domain.Services;
+using MVC_Project.Utils;
+using MVC_Project.WebBackend.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +13,15 @@ namespace MVC_Project.WebBackend.Controllers
 {
     public class RegisterController : BaseController
     {
+        private IUserService _userService;
+        private IRoleService _roleService;
+
+        public RegisterController(IUserService userService, IRoleService roleService)
+        {
+            _userService = userService;
+            _roleService = roleService;
+        }
+
         [AllowAnonymous]
         public ActionResult Index()
         {
@@ -20,11 +33,52 @@ namespace MVC_Project.WebBackend.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult CreateAccount(RegisterViewModel model)
         {
+            if (!String.IsNullOrWhiteSpace(model.ConfirmPassword)
+                && !String.IsNullOrWhiteSpace(model.Password))
+            {
+                if (!model.Password.Equals(model.ConfirmPassword))
+                {
+                    ModelState.AddModelError("ConfirmPassword", "Las contraseñas no coinciden");
+                }
+            }
+
             if (ModelState.IsValid)
             {
+                DateTime todayDate = DateUtil.GetDateTimeNow();
 
+                string daysToExpirateDate = ConfigurationManager.AppSettings["DaysToExpirateDate"];           
+                DateTime passwordExpiration = todayDate.AddDays(Int32.Parse(daysToExpirateDate));
+
+                var availableRoles = _roleService.GetAll();
+                var role = availableRoles.Where(x => x.Code == "EMPLOYEE").FirstOrDefault();
+
+                var user = new User
+                {
+                    Uuid = Guid.NewGuid().ToString(),
+                    FirstName = model.FistName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Password = SecurityUtil.EncryptPassword(model.Password),
+                    PasswordExpiration = passwordExpiration,
+                    Username = model.Email,
+                    CreatedAt = todayDate,
+                    UpdatedAt = todayDate,
+                    Status = true,
+                    Role = new Role { Id = role.Id }
+                };
+                
+                foreach (var permission in role.Permissions)
+                {
+                    user.Permissions.Add(permission);
+                }
+                _userService.Create(user);
+                ViewBag.Message = "Usuario registrado";
+                return RedirectToAction("Login", "Auth");
             }
-            return View(model);
+            else
+            {                
+                return View("Index", model);
+            }           
         }
                 
     }
