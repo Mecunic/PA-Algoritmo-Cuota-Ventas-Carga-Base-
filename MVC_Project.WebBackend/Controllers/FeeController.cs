c92339a0fad8f29f00b8072353fb57f92ed5544c
Alex Vieyra <alejandro.vieyra@ensitech.com>
2021-01-13 12:46:11 -0600

    Vistas de Cálculo y Seguimiento

---
+++ MVC_Project.WebBackend/Controllers/FeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.WebBackend.Controllers
+{    
+    public class FeeController : Controller
+    {
+        // GET: Fee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calculate()
+        {
+            return View();
+        }
+
+        public ActionResult Tracing()
+        {
+            return View();
+        }
+    }
+}
