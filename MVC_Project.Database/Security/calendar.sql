306a91aa5da718a4b19bbb226f12551c22372d58
Michell.Cuni <michell.cuni@ensitech.com>
2021-02-26 13:30:01 -0600

    SinReq Se eliminan los proyectos que no seran utilizados de la plantilla

--- MVC_Project.Database/Security/calendar.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE SCHEMA [calendar]
-    AUTHORIZATION [dbo];
-
--- MVC_Project.Database/calendar/Tables/event_categories.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [calendar].[event_categories] (
-    [eventCalendarId] BIGINT        IDENTITY (1, 1) NOT NULL,
-    [name]            VARCHAR (255) NULL,
-    [bgcolor]         VARCHAR (100) NULL,
-    PRIMARY KEY CLUSTERED ([eventCalendarId] ASC)
-);
-
