a5910b129fd72aea566ea3dde425f2f3a5b94c8e
Cesar Couoh <cesar@ensitech.com>
2021-02-26 12:18:57 -0600

    Req14 Parametros | LOC: 109| Pantalla dashboard de parametros , vista en el menu

---
+++ MVC_Project.WebBackend/Controllers/ParametrosController.cs
@@ -0,0 +1,59 @@
+﻿using MVC_Project.WebBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.WebBackend.Controllers
+{
+    public class ParametrosController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAllByFilter(JQueryDataTableParams param, string filtros)
+        {
+            try
+            {
+                NameValueCollection filtersValues = HttpUtility.ParseQueryString(filtros);
+                //var results = _productoService.FilterBy(filtersValues, param.iDisplayStart, param.iDisplayLength);
+                IList<ParametroModel> dataResponse = new List<ParametroModel>();
+                /*foreach (var prod in results.Item1)
+                {
+                    ProductoModel productoModel = new ProductoModel();
+                    productoModel.SKU = prod.SKU;
+                    productoModel.TipoEmpaque = prod.TipoEmpaque?.Name;
+                    productoModel.Presentacion = prod.Presentacion.Name;
+                    productoModel.PrecioUnitario = prod.PrecioUnitario;
+                    productoModel.Uuid = prod.Uuid;
+                    productoModel.Status = prod.Status;
+                    productoModel.Descripcion = prod.Descripcion;
+                    dataResponse.Add(productoModel);
+                }*/
+                return Json(new
+                {
+                    success = true,
+                    param.sEcho,
+                    iTotalRecords = dataResponse.Count(),
+                    iTotalDisplayRecords = dataResponse.Count(),//results.Item2,
+                    aaData = dataResponse
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult
+                {
+                    Data = new { Mensaje = new { title = "Error", message = ex.Message } },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                    MaxJsonLength = Int32.MaxValue
+                };
+            }
+        }
+
+    }
+}
---
+++ MVC_Project.WebBackend/Models/ParametrosViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project.WebBackend.Models
+{
+    public class ParametrosViewModel : DataTableModel
+    {
+        [Display(Name = "Filtro")]
+        public string Filtro { get; set; }
+    }
+    
+    public class ParametroModel
+    {
+        public string Clave { get; set; }
+        public string Parametro { get; set; }
+        public string Valor { get; set; }
+        public string Tipo { get; set; }
+        public string AlgoritmoUso { get; set; }
+    }
+
+}
